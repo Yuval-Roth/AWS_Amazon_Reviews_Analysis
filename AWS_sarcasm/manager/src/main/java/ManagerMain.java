45daa5abba78d6c89bc4d258d9243aefe3cde5f4
adiaybgu <102467324+adiaybgu@users.noreply.github.com>
2024-01-28 16:52:23 +0200

    empty main

--- AWS_sarcasm/manager/src/main/java/ManagerMain.java
+++ AWS_sarcasm/manager/src/main/java/ManagerMain.java
@@ -12,39 +12,37 @@ public class ManagerMain {
     private static final String SQS_DOMAIN_PREFIX = "https://sqs.us-east-1.amazonaws.com/057325794177/";
     private static Region ec2_region = Region.US_EAST_1;
     private static Region s3_region = Region.US_WEST_2;
+
     public static void main(String[] args) {
+    }
+
+    private static void tempMain() {
         String queueName = "user-input";
         ReceiveMessageRequest messageRequest = ReceiveMessageRequest.builder()
                 .maxNumberOfMessages(1)
                 .queueUrl(SQS_DOMAIN_PREFIX + queueName + ".fifo")
                 .build();
 
-       SqsClient sqs = SqsClient.builder()
+        SqsClient sqs = SqsClient.builder()
                 .region(ec2_region)
                 .build();
 
-       var r =  sqs.receiveMessage(messageRequest);
-       String message = r.messages().getFirst().body().toString();
-       String[] messages = message.split("\n");
-        for (String json:messages) {
-            int counterReviews  = 0;
+        var r = sqs.receiveMessage(messageRequest);
+        String message = r.messages().getFirst().body();
+        String[] messages = message.split("\n");
+        for (String json : messages) {
+            int counterReviews = 0;
             List<TitleReviews> titleReviewsList = new LinkedList<>();
-            TitleReviews tr = JsonUtils.deserialize(json,TitleReviews.class);
+            TitleReviews tr = JsonUtils.deserialize(json, TitleReviews.class);
             for (:
-                 ) {
-                
+                 ){
+
             }
-            
-                    
-            
 
         }
 
-
-
-
-
     }
-
-
 }
+
+
+
